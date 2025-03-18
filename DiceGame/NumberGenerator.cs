ece7a97dd506816f9529c4142dec9c564982cafc
Yauheni Valiantsinovich <zhenya.val.77@gmail.com>
2025-03-18 23:48:06 +0300

    Add ganerating if number of random 0..1 and 0..5

--- DiceGame/NumberGenerator.cs
+++ DiceGame/NumberGenerator.cs
@@ -1,6 +1,18 @@
+using System.Security.Cryptography;
+
 namespace DiceGame;
 
 public class NumberGenerator
 {
-    
+    public static int GenerateNumber(int min, int max)
+    {
+        return RandomNumberGenerator.GetInt32(min, max);
+    }
+
+    /*public static byte[] GenerateSecretKeyNumber()
+    {
+        byte[] secretKey = new byte[32];
+        
+        return RandomNumberGenerator.Fill(secretKey);
+    }*/
 }
